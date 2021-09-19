c98bb238cd262f830d52865816ed98cad1475eea
Shekhar Tarare <shekhartarare@gmail.com>
2021-09-19 13:20:03 +0530

    Setup ViewModels and AutoMapper

--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using leave_management.Contracts;
 using leave_management.Data;
+using leave_management.Mappings;
 using leave_management.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -36,6 +38,10 @@ namespace leave_management
             services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
             services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
             services.AddScoped<ILeaveHistoryRepository, LeaveHistoryRepository>();
+
+            //For AutoMapper
+            services.AddAutoMapper(typeof(Maps));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
